f1a528cdfeb79b912c63866c5ff6610c7835916d
Muhammad Fahad <fahad2512001@gmail.com>
2025-12-26 05:14:12 +0300

    Change news fetching URL to a placeholder
    Updated the news fetching URL placeholder.

--- news_fetcher.py
+++ news_fetcher.py
@@ -3,7 +3,7 @@ from config import CRYPTO_PANIC_API_KEY
 from notifier import send_to_telegram
 
 def fetch_news():
-    url = f"https://cryptopanic.com/api/developer/v2/posts/?auth_token=b9b125d3d6aab48634e50192e39268635170786f&filter=important"
+    url = f"Your URL here"
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -18,3 +18,4 @@ def fetch_news():
         print(f"Error fetching news: {e}")
         send_to_telegram("wait don’t get too hyped or make wrong decision")
         return []
+
