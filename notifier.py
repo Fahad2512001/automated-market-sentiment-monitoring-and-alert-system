1859206a7d5245605afc4650e17e9750dcef6526
Muhammad Fahad <fahad2512001@gmail.com>
2025-12-26 05:15:36 +0300

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -2,7 +2,7 @@ import requests
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
 def send_to_telegram(message):
-    url = f"https://api.telegram.org/bot8179488895:AAGRCUnQgeci5b8dxOEwdFLiHGczSB1nY2E/sendMessage"
+    url = f"Your URL Here"
     payload = {
         "chat_id": TELEGRAM_CHAT_ID,
         "text": message
@@ -12,3 +12,4 @@ def send_to_telegram(message):
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"Failed to send Telegram message: {e}")
+
